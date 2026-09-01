95086a3f9b5c4ee2cea263f9280ec88a118b4bef
Tushar Verma <tv161228@gmail.com>
2026-09-01 18:16:44 +0530

    Update App routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Signup from "./pages/auth/Signup";
-
 export default function App() {
   return (
     <BrowserRouter>
